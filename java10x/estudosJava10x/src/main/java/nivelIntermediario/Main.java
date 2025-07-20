c78b142f8b2e086e6472a7a57bfa7f5f37ca1e26
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-07-20 15:30:37 -0300

    alguns arquivos deletados

--- java10x/estudosJava10x/src/main/java/nivelIntermediario/Main.java
+++ java10x/estudosJava10x/src/main/java/nivelIntermediario/Main.java
@@ -1,14 +1,23 @@
 package nivelIntermediario;
 
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
+        Set<String> ninjasSet = new HashSet<>();
 
-        ArrayList<String> arrayList = new ArrayList<>();
-        LinkedList<String> linkedList = new LinkedList<>();
+        ninjasSet.add("Sasuke Uchiha");
+        ninjasSet.add("Sasuke Uchiha");
+        ninjasSet.add("Sakura Haruno");
+
+
+        System.out.println("Ninjas set: " + ninjasSet);
+
+        Set<String> ninjasTreeSet = new HashSet<>();
+
 
     }
 }
